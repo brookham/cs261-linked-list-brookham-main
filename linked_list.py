efef4144192e3b7e4353bb7dc03748273d7a2b98
brookham <bhamilton3264@gmail.com>
2025-02-04 08:52:18 -0800

    got a good start

---
+++ linked_list.py
@@ -0,0 +1,47 @@
+# LinkedList: A doubly-linked list.
+# Bonus: Has an insert_in_order that, when used, keeps the values of
+#        each node in ascending order.
+# Implement as many operations as possible with recursion.
+# If you can't figure it out recursively, use a loop. (But then refactor
+# your implementation into a recursive one!)
+# Your implementation should pass the tests in test_sorted_list.py.
+
+# Brook Hamilton
+# Collaborators:
+# Time Spent: 
+
+class LinkedList:
+   
+    def __init__(self, value = None):
+        self.value = value
+        self.next = self
+        self.previous = self
+
+    def is_sentinel(self):
+        if self.value == None:
+            return True
+        else:
+            return False
+    
+    def is_empty(self):
+        if self.next == self and self.previous == self:
+            return True
+        else:
+            return False
+        
+    def is_last_node(self):
+        while not(self.is_empty() or self.next.is_sentinel):
+            self = self.next
+        return self
+    
+    def last_node(self):
+        if not (self.is_empty() or self.next.is_sentinel()):
+            self.last_node(self.next)
+        return self
+    
+    def append(self):
+        if not(self.next.is_sentinel()):
+            self = self.next
+        
+
+
